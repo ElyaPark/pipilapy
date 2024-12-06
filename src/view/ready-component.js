e7b25ee1672b79d6220875250f6f0778b15511ad
ElyaPark <elinablagodatskaya1@gmail.com>
2024-12-06 22:43:35 +0300

    Update ready-component.js

--- src/view/ready-component.js
+++ src/view/ready-component.js
@@ -1,40 +1,39 @@
-import {createElement} from '../framework/render.js';
+import { AbstractComponent } from "../framework/view/abstract-component.js";
 
-
-function createHeaderComponentTemplate() {
-    return (
-      
-      `
- <div class="column">
-                <h2>Готово</h2>
-                <ul id="done" class="task-list">
-                    <li>Позвонить маме</li>
-                    <li>Погладить кота</li>
-                </ul>
-            </div>
-    `
-
-      );
+function createReadyTemplate() {
+  return `
+    <div class="column" data-status="done">
+      <h2>Готово</h2>
+      <ul class="task-list"></ul>
+    </div>
+  `;
 }
 
-
-export default class HeaderComponent {
-  getTemplate() {
-    return createHeaderComponentTemplate();
+export default class ReadyComponent extends AbstractComponent {
+  get template() {
+    return createReadyTemplate();
   }
 
+  get element() {
+    if (!this.elementInstance) {
+      this.elementInstance = super.element;
 
-  getElement() {
-    if (!this.element) {
-      this.element = createElement(this.getTemplate());
+      this.elementInstance.addEventListener("dragover", (event) => {
+        event.preventDefault(); // Разрешаем сброс
+      });
+
+      this.elementInstance.addEventListener("drop", (event) => {
+        const taskId = event.dataTransfer.getData("text/plain");
+        const status = "done";
+        if (this.onDrop) {
+          this.onDrop(taskId, status); // Вызываем обработчик drop
+        }
+      });
     }
-
-
-    return this.element;
+    return this.elementInstance;
   }
 
-
-  removeElement() {
-    this.element = null;
+  setOnDropHandler(handler) {
+    this.onDrop = handler;
   }
 }
