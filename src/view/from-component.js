42d4f0396df98ba4e9677e3fa29e1dd4637e384d
ElyaPark <elinablagodatskaya1@gmail.com>
2024-12-06 22:21:43 +0300

    Update from-component.js

--- src/view/from-component.js
+++ src/view/from-component.js
@@ -1,40 +1,49 @@
-import {createElement} from '../framework/render.js';
+import { AbstractComponent } from "../framework/view/abstract-component.js";
 
-
-function createHeaderComponentTemplate() {
-    return (
-      
-      `
-
-        <div class="task-input">
-            <label for="new-task">Новая задача</label>
-            <input type="text" id="new-task" placeholder="Название задачи...">
-            <button id="add-task">+ Добавить</button>
-         </div>
-
-    `
-
-      );
+function createFormTemplate() {
+  return `
+    <div class="task-input">
+      <label for="new-task">Новая задача</label>
+      <input type="text" id="new-task" placeholder="Название задачи...">
+      <button id="add-task">+ Добавить</button>
+    </div>
+  `;
 }
 
-
-export default class HeaderComponent {
-  getTemplate() {
-    return createHeaderComponentTemplate();
+export default class FormComponent extends AbstractComponent {
+  constructor({ onAddTask }) {
+    super();
+    this.onAddTask = onAddTask;
   }
 
+  get element() {
+    if (!this.elementInstance) {
+      this.elementInstance = super.element;
 
-  getElement() {
-    if (!this.element) {
-      this.element = createElement(this.getTemplate());
+      const inputField = this.elementInstance.querySelector("#new-task");
+      const addButton = this.elementInstance.querySelector("#add-task");
+
+      addButton.addEventListener("click", (event) => {
+        event.preventDefault(); // Предотвращаем перезагрузку страницы
+        const taskTitle = inputField.value.trim();
+
+        if (taskTitle && this.onAddTask) {
+          this.onAddTask(taskTitle); // Вызываем переданный обработчик
+          this.clearInput(); // Очищаем поле после добавления задачи
+        }
+      });
     }
-
-
-    return this.element;
+    return this.elementInstance;
   }
 
-
-  removeElement() {
-    this.element = null;
+  get template() {
+    return createFormTemplate();
+  }
+ф
+  clearInput() {
+    const inputField = this.element.querySelector("#new-task");
+    if (inputField) {
+      inputField.value = "";
+    }
   }
 }
