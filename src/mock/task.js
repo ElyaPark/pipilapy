b0fa16fa64ea72bf297ec6655338e10a8aeb574f
ElyaPark <elinablagodatskaya1@gmail.com>
2024-12-06 22:51:19 +0300

    Update task.js

---
+++ src/mock/task.js
@@ -0,0 +1,9 @@
+export const tasks = [
+    { id: 1, title: 'Task 1', status: 'backlog' },
+    { id: 2, title: 'Task 2', status: 'processing' },
+    { id: 3, title: 'Task 3', status: 'done' },
+    { id: 4, title: 'Task 4', status: 'basket' },
+    { id: 5, title: 'Task 5', status: 'backlog' },
+    // Добавьте больше задач при необходимости
+  ];
+  
