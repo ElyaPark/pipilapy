ba1ca1520231aaedf56ad69e55a4dc8179e295fd
ElyaPark <elinablagodatskaya1@gmail.com>
2024-12-06 22:52:16 +0300

    Update render.js

--- src/framework/render.js
+++ src/framework/render.js
@@ -1,24 +1,26 @@
-const RenderPosition = {
-    BEFOREBEGIN: 'beforebegin',
-    AFTERBEGIN: 'afterbegin',
-    BEFOREEND: 'beforeend',
-    AFTEREND: 'afterend',
-  };
-  
-  
-  function createElement(template) {
-    const newElement = document.createElement('div');
-    newElement.innerHTML = template;
-  
-  
-    return newElement.firstElementChild;
+import { AbstractComponent } from "./view/abstract-component.js";
+
+export const RenderPosition = {
+  BEFOREBEGIN: "beforebegin",
+  AFTERBEGIN: "afterbegin",
+  BEFOREEND: "beforeend",
+  AFTEREND: "afterend",
+};
+
+export function createElement(template) {
+  const newElement = document.createElement("div");
+  newElement.innerHTML = template;
+  return newElement.firstElementChild;
+}
+
+export function render(component, container, place = RenderPosition.BEFOREEND) {
+  if (!(component instanceof AbstractComponent)) {
+    throw new Error("Can render only components");
   }
-  
-  
-  function render(component, container, place = RenderPosition.BEFOREEND) {
-  container.insertAdjacentElement(place, component.getElement());
+
+  if (!container) {
+    throw new Error("Container element doesn't exist");
   }
-  
-  
-  export {RenderPosition, createElement, render};
-  
+
+  container.insertAdjacentElement(place, component.element);
+}
