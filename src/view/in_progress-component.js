2d39059aef8109e1998b42c195d39ecc43ca4b6b
ElyaPark <elinablagodatskaya1@gmail.com>
2024-12-07 23:54:53 +0300

    Update in_progress-component.js

--- src/view/in_progress-component.js
+++ src/view/in_progress-component.js
@@ -19,14 +19,14 @@ export default class InProgressComponent extends AbstractComponent {
       this.elementInstance = super.element;
 
       this.elementInstance.addEventListener("dragover", (event) => {
-        event.preventDefault(); // Разрешаем сброс
+        event.preventDefault(); 
       });
 
       this.elementInstance.addEventListener("drop", (event) => {
         const taskId = event.dataTransfer.getData("text/plain");
         const status = "processing";
         if (this.onDrop) {
-          this.onDrop(taskId, status); // Вызываем обработчик drop
+          this.onDrop(taskId, status); 
         }
       });
     }
