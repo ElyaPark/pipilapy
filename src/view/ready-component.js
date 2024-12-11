cccfb21ae319ed5070a1510ecf8625fb4771ea7f
ElyaPark <elinablagodatskaya1@gmail.com>
2024-12-11 12:05:12 +0300

    Update ready-component.js

--- src/view/ready-component.js
+++ src/view/ready-component.js
@@ -19,14 +19,14 @@ export default class ReadyComponent extends AbstractComponent {
       this.elementInstance = super.element;
 
       this.elementInstance.addEventListener("dragover", (event) => {
-        event.preventDefault(); 
+        event.preventDefault(); // Разрешаем сброс
       });
 
       this.elementInstance.addEventListener("drop", (event) => {
         const taskId = event.dataTransfer.getData("text/plain");
         const status = "done";
         if (this.onDrop) {
-          this.onDrop(taskId, status); 
+          this.onDrop(taskId, status); // Вызываем обработчик drop
         }
       });
     }
