7fedf2da974c56faa0e71794230d1144dd89ac3b
ElyaPark <elinablagodatskaya1@gmail.com>
2024-12-11 12:03:34 +0300

    Update from-component.js

--- src/view/from-component.js
+++ src/view/from-component.js
@@ -24,12 +24,12 @@ export default class FormComponent extends AbstractComponent {
       const addButton = this.elementInstance.querySelector("#add-task");
 
       addButton.addEventListener("click", (event) => {
-        event.preventDefault(); 
+        event.preventDefault(); // Предотвращаем перезагрузку страницы
         const taskTitle = inputField.value.trim();
 
         if (taskTitle && this.onAddTask) {
-          this.onAddTask(taskTitle); 
-          this.clearInput(); 
+          this.onAddTask(taskTitle); // Вызываем переданный обработчик
+          this.clearInput(); // Очищаем поле после добавления задачи
         }
       });
     }
