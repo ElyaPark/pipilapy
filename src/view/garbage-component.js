cb9690ea8cf6b5b8b4826dffd6a8333a3b2c48ff
ElyaPark <elinablagodatskaya1@gmail.com>
2024-12-11 12:03:46 +0300

    Update garbage-component.js

--- src/view/garbage-component.js
+++ src/view/garbage-component.js
@@ -20,21 +20,21 @@ export default class GarbageComponent extends AbstractComponent {
       this.elementInstance = super.element;
 
       this.elementInstance.addEventListener("dragover", (event) => {
-        event.preventDefault(); 
+        event.preventDefault(); // Разрешаем сброс
       });
 
       this.elementInstance.addEventListener("drop", (event) => {
         const taskId = event.dataTransfer.getData("text/plain");
         const status = "basket";
         if (this.onDrop) {
-          this.onDrop(taskId, status); 
+          this.onDrop(taskId, status); // Вызываем обработчик drop
         }
       });
 
       const resetButton = this.elementInstance.querySelector(".reset-button");
       resetButton.addEventListener("click", () => {
         if (this.onClear) {
-          this.onClear(); 
+          this.onClear(); // Вызываем обработчик очистки
         }
       });
     }
@@ -52,7 +52,7 @@ export default class GarbageComponent extends AbstractComponent {
   toggleClearButtonState(hasTasks) {
     const resetButton = this.element.querySelector(".reset-button");
     if (resetButton) {
-      resetButton.disabled = !hasTasks; 
+      resetButton.disabled = !hasTasks; // Включаем или отключаем кнопку
     }
   }
 }
