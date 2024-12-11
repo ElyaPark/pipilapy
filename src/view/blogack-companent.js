ce7073810ff0e6bb9a2e0dadc121e96d0aeef7b9
ElyaPark <elinablagodatskaya1@gmail.com>
2024-12-11 12:03:30 +0300

    Update blogack-companent.js

--- src/view/blogack-companent.js
+++ src/view/blogack-companent.js
@@ -19,14 +19,14 @@ export default class BlogackComponent extends AbstractComponent {
       this.elementInstance = super.element;
 
       this.elementInstance.addEventListener("dragover", (event) => {
-        event.preventDefault(); 
+        event.preventDefault(); // Разрешаем сброс
       });
 
       this.elementInstance.addEventListener("drop", (event) => {
         const taskId = event.dataTransfer.getData("text/plain");
         const status = "backlog";
         if (this.onDrop) {
-          this.onDrop(taskId, status); 
+          this.onDrop(taskId, status); // Вызываем обработчик drop
         }
       });
     }
