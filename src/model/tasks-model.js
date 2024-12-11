4325faa28a2829dca11ead4c689b8378d5564d87
ElyaPark <elinablagodatskaya1@gmail.com>
2024-12-11 12:03:18 +0300

    Update tasks-model.js

--- src/model/tasks-model.js
+++ src/model/tasks-model.js
@@ -1,17 +1,63 @@
-import { render } from "../framework/render.js";
-import TaskComponent from "./view/blogack-companent.js";
+import Observable from '../framework/observable.js';
 
-export default class TaskPresenter {
-  #task = null;
-  #tasksListComponent = null;
+export default class TasksModel extends Observable {
+  #tasksApiService = null;
+  #boardtasks = [];
 
-  constructor(task, tasksListComponent) {
-    this.#task = task;
-    this.#tasksListComponent = tasksListComponent;
+  constructor({ tasksApiService }) {
+    super();
+    this.#tasksApiService = tasksApiService;
   }
 
-  init() {
-    const taskComponent = new TaskComponent(this.#task);
-    render(taskComponent, this.#tasksListComponent.element);
+  get tasks() {
+    return this.#boardtasks;
+  }
+
+  async init() {
+    try {
+      const tasks = await this.#tasksApiService.tasks;
+      this.#boardtasks = tasks;
+    } catch (err) {
+      this.#boardtasks = [];
+      console.error('Ошибка загрузки задач:', err);
+    }
+    this._notify('INIT'); // Уведомляем об успешной загрузке
+  }
+
+  getTasksByStatus(status) {
+    return this.#boardtasks.filter((task) => task.status === status);
+  }
+
+  async addTask(task) {
+    try {
+      const newTask = await this.#tasksApiService.addTask(task);
+      this.#boardtasks.push(newTask);
+      this._notify('ADD_TASK', newTask);
+    } catch (err) {
+      console.error('Ошибка добавления задачи:', err);
+    }
+  }
+
+  async updateTask(task) {
+    try {
+      const updatedTask = await this.#tasksApiService.updateTask(task);
+      const index = this.#boardtasks.findIndex((item) => item.id === task.id);
+      if (index !== -1) {
+        this.#boardtasks[index] = updatedTask;
+        this._notify('UPDATE_TASK', updatedTask);
+      }
+    } catch (err) {
+      console.error('Ошибка обновления задачи:', err);
+    }
+  }
+
+  async deleteTask(taskId) {
+    try {
+      await this.#tasksApiService.deleteTask(taskId);
+      this.#boardtasks = this.#boardtasks.filter((task) => task.id !== taskId);
+      this._notify('DELETE_TASK', taskId);
+    } catch (err) {
+      console.error('Ошибка удаления задачи:', err);
+    }
   }
 }
