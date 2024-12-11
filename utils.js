cf710ad4d21d865a0e847565094d1094cc609824
ElyaPark <elinablagodatskaya1@gmail.com>
2024-12-11 12:05:25 +0300

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,4 +1,3 @@
 export function generateUniqueId() {
-    return Date.now().toString(36) + Math.random().toString(36).substring(2, 15);
-  }
-  
+  return Date.now().toString(36) + Math.random().toString(36).substring(2, 15);
+}
